148fdf34c5313d2e202d13a989aec929cd42ac52
zuiwuchang <zuiwuchang@gmail.com>
2022-12-26 17:36:03 +0800

    rw

--- README.zh.md
+++ README.zh.md
@@ -5,22 +5,24 @@ deno sqlite3 async
 [English](README.md)
 
 [x/sqlite](https://deno.land/x/sqlite) 是一個 deno 的 sqlite 庫，它使用了
-WebAssembly 因此很容器嵌入到不同 os 平臺，然而
+WebAssembly 因此很容易嵌入到不同 os 平臺，然而
 [x/sqlite](https://deno.land/x/sqlite) 不支持異步
-api，於是我對齊進行了二次開發，將對 [x/sqlite](https://deno.land/x/sqlite)
-的調用都放到一個單獨的 web worker 中，而在主線程中以直接以 異步 api 提供接口
+api，於是我對其進行了二次開發，將對 [x/sqlite](https://deno.land/x/sqlite)
+的調用都放到一個單獨的 web worker 中，而在主線程中直接以 異步 api 提供接口
 
 # RawDB
 
 class RawDB 是對 [x/sqlite](https://deno.land/x/sqlite)
 的異步封裝，如果你想得到一個幾乎和 [x/sqlite](https://deno.land/x/sqlite)
 一致但已經封裝好的異步庫則可以使用 RawDB，或者你想對
-[x/sqlite](https://deno.land/x/sqlite) 進行異步接口的封裝也可以從 RawDB
+[x/sqlite](https://deno.land/x/sqlite) 進行異步接口的自定義封裝也可以從 RawDB
 開始，否則你應該使用 [class DB](#DB)
 
 RawDB 大部分 api 和 [x/sqlite](https://deno.land/x/sqlite) 類似，不同在於 RawDB
 提供了一個 batch 函數用於將多組命令批量傳遞給 web worker 執行用於縮短與 web
 worker 通信產生的延遲
+
+你可以查看這個 [例子](bin/raw.ts) 來了解更多細節
 
 # DB
 
---
+++ internal/rw.ts
@@ -0,0 +1,155 @@
+import { Chan, selectChan, WriteChannel } from "../deps/easyts/mod.ts";
+import { background, Context } from "../deps/easyts/context/mod.ts";
+export class RW {
+  private wl_ = new Chan<number>();
+  private wu_ = new Chan<number>();
+  private rl_ = new Chan<number>();
+  private ru_ = new Chan<number>();
+  private done_ = new Chan<number>();
+  private w_ = false;
+  private r_ = 0;
+  constructor() {
+    this.serve();
+  }
+  close() {
+    this.done_.close();
+  }
+  async serve() {
+    const done = this.done_;
+    const wl = this.wl_;
+    const wu = this.wu_;
+    const rl = this.rl_;
+    const ru = this.ru_;
+
+    const cdone = done.readCase();
+    const cwl = wl.readCase();
+    const cwu = wu.readCase();
+    const crl = rl.readCase();
+    const cru = ru.readCase();
+
+    while (true) {
+      if (this.w_) { // write lock wait unlock
+        switch (await selectChan(cdone, cwu)) {
+          case cwu:
+            if (!this.w_) {
+              throw new Error(`RW.unlock on w=${this.w_}`);
+            } else if (this.r_ != 0) {
+              throw new Error(`RW.unlock on r=${this.r_}`);
+            }
+            this.w_ = false;
+            break;
+          default:
+            return;
+        }
+      } else if (this.r_ > 0) { // read lock wait unlock or mor read lock
+        switch (await selectChan(cdone, crl, cru)) {
+          case crl:
+            if (this.w_) {
+              throw new Error(`RW.readLock on w=${this.w_}`);
+            }
+            this.r_++;
+            break;
+          case cru:
+            if (this.w_) {
+              throw new Error(`RW.readUnlock on w=${this.w_}`);
+            } else if (this.r_ < 1) {
+              throw new Error(`RW.readUnlock on r=${this.r_}`);
+            }
+            this.r_--;
+            break;
+          default:
+            return;
+        }
+      } else { // no lock wait write/read lock
+        switch (await selectChan(cdone, cwl, crl)) {
+          case cwl:
+            if (this.w_) {
+              throw new Error(`RW.lock on w=${this.w_}`);
+            } else if (this.r_ > 0) {
+              throw new Error(`RW.lock on r=${this.r_}`);
+            }
+            this.w_ = true;
+            break;
+          case crl:
+            if (this.w_) {
+              throw new Error(`RW.lock on w=${this.w_}`);
+            }
+            this.r_++;
+            break;
+          default:
+            return;
+        }
+      }
+    }
+  }
+  async _send(ctx: Context, ch: WriteChannel<number>): Promise<boolean> {
+    if (ctx.isClosed) {
+      return false;
+    }
+    const done = this.done_;
+    if (done.isClosed) {
+      throw new Error("RW already closed");
+    }
+    const cdone = done.readCase();
+    const cd = ctx.done.readCase();
+    const cw = ch.writeCase(0);
+    switch (await selectChan(cdone, cd, cw)) {
+      case cdone:
+        throw new Error("RW already closed");
+      case cd:
+        return false;
+      case cw:
+        break;
+    }
+    return true;
+  }
+  async lock(ctx: Context): Promise<Locked | undefined> {
+    const ok = await this._send(ctx, this.wl_);
+    if (ok) {
+      return new _Locked(this);
+    }
+  }
+  /**
+   * @internal
+   */
+  unlock(ctx: Context): Promise<boolean> {
+    return this._send(ctx, this.wu_);
+  }
+  async readLock(ctx: Context): Promise<Locked | undefined> {
+    const ok = await this._send(ctx, this.rl_);
+    if (ok) {
+      return new _Locked(this, true);
+    }
+  }
+  /**
+   * @internal
+   */
+  readUnlock(ctx: Context): Promise<boolean> {
+    return this._send(ctx, this.ru_);
+  }
+}
+export interface Locked {
+  unlock(): void;
+}
+class _Locked {
+  constructor(readonly rw: RW, readonly read?: boolean) {}
+  private ok = true;
+  unlock() {
+    if (this.ok) {
+      this.ok = false;
+      this._unlock();
+    } else {
+      throw new Error(`RW already unlocked`);
+    }
+  }
+  async _unlock() {
+    try {
+      if (this.read) {
+        await this.rw.readUnlock(background());
+      } else {
+        await this.rw.unlock(background());
+      }
+    } catch (_) { //
+    }
+  }
+}
